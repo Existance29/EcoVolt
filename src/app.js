9ddbc73de83c4a31354e756a7006d8087749f567
NgJoeYi <s10262850@connect.np.edu.sg>
2024-10-19 19:36:12 +0800

    index.html is up

---
+++ src/app.js
@@ -0,0 +1,41 @@
+//import
+const express = require("express");
+const app = express()
+const port = process.env.PORT || 3000
+const dbConfig = require("./database/dbConfig")
+const sql = require("mssql")
+// const route = require("./routes/routes")
+const bodyParser = require("body-parser")
+
+//load frontend
+const staticMiddleware = express.static("public")
+app.use(staticMiddleware)
+
+//use parse middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//setup routes
+// route(app)
+
+app.listen(port, async () => {
+  try {
+    // Connect to the database
+    await sql.connect(dbConfig)
+    console.log("Connected to database successfully")
+  } catch (err) {
+    console.error("Database connection error:", err)
+    // Terminate app
+    process.exit(1)
+  }
+
+  console.log(`Server listening on port ${port}`);
+});
+
+// Close the connection pool on SIGINT signal
+process.on("SIGINT", async () => {
+  console.log("Server is shutting down")
+  await sql.close()
+  console.log("Database connection closed")
+  process.exit(0) 
+});
---
+++ src/public/scripts/common.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("nav").load("commonHTML/navBar.html");
+    $("footer").load("commonHTML/footer.html");
+});
