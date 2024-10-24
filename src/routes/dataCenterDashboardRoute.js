d23f0228dbf6dd922551e6653fac151798022d22
NgJoeYi <s10262850@connect.np.edu.sg>
2024-10-25 06:13:04 +0800

    added parameters to DC routes

--- src/routes/dataCenterDashboardRoute.js
+++ src/routes/dataCenterDashboardRoute.js
@@ -1,8 +1,8 @@
 const dataCenterDashboardController = require('../controllers/dataCenterDashboardController.js');
 
-const dataCenterDashboardRoute = (app) => {
-    app.get('/dataCenterDashboard/carbonEmissions', dataCenterDashboardController.getAllCarbonEmissionsData);
-    app.get('/dataCenterDashboard/sustainabilityGoals', dataCenterDashboardController.getAllSustainabilityGoalsData);    
+const dataCenterDashboardRoute = (app) => {   
+    app.get('/api/carbon-emissions/:company_id/:date', dataCenterDashboard.getAllCarbonEmissionsData);
+    app.get('/api/sustainability-goals/:company_id/:date', dataCenterDashboard.getAllSustainabilityGoalsData);
 }
 
 module.exports = dataCenterDashboardRoute;
