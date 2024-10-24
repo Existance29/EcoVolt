fc5bb5a830121e6464cdbb40ae56bfe95d31400b
exihs <54537576+Existance29@users.noreply.github.com>
2024-10-24 19:13:58 +0800

    sign up frontend

--- src/public/scripts/common.js
+++ src/public/scripts/common.js
@@ -2,3 +2,18 @@ $(document).ready(function() {
     $("nav").load("commonHTML/navBar.html");
     $("footer").load("commonHTML/footer.html");
 });
+
+//prevent reloading page when form submitted
+document.addEventListener("DOMContentLoaded", function () {
+    //get all forms
+    const forms = document.getElementsByTagName("form")
+    //add listener to trigger when submitted
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        //stop reloading behaviour
+        event.preventDefault()
+      }, false)
+    })
+    
+  
+  })
