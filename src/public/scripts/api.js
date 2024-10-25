6ee3c8650513604ca022dc9e64ce5d2d03fe0954
exihs <54537576+Existance29@users.noreply.github.com>
2024-10-25 08:28:47 +0800

    frontend functions

---
+++ src/public/scripts/api.js
@@ -0,0 +1,53 @@
+//functions for CRUD api calls
+
+const accessToken = localStorage.getItem("accessToken") || sessionStorage.getItem("accessToken");
+
+//check if user is logged in 
+async function isLoggedIn(){
+    if (accessToken === null) return false
+    //make sure the jwt is valid
+    const response = await get("/users/decodejwt")
+    return response.status == 200
+}
+
+async function post(url, jsondata){
+    let settings = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "cache-control": "no-cache",
+        "authorization": `Bearer ${accessToken}`
+      },
+  
+      body: JSON.stringify(jsondata)
+    }
+    return await fetch(url, settings)
+    
+}
+
+async function get(url){
+    let settings = {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        "authorization": `Bearer ${accessToken}`
+      }
+    }
+    return await fetch(url, settings)
+  
+}
+
+async function put(url, jsondata){
+  let settings = {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+      "cache-control": "no-cache",
+      "authorization": `Bearer ${accessToken}`
+    },
+    body: JSON.stringify(jsondata)
+  }
+
+  return await fetch(url, settings)
+  
+}
---
+++ src/public/scripts/utils.js
@@ -0,0 +1,19 @@
+//a bunch of utility functions
+
+function getCompanyID(){
+    return localStorage.getItem("companyId") || sessionStorage.getItem("companyId");
+}
+
+//get the value of the url parameter of the current address
+function getUrlParameter(sParam){
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return decodeURI(sParameterName[1]);
+        }
+    }
+}
