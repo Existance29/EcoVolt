f7cf74e63c98c93d1f6c2f490fa1d5372a3ddc26
NgJoeYi <s10262850@connect.np.edu.sg>
2024-10-26 05:01:49 +0800

    added new routes

--- src/routes/dataCenterDashboardRoute.js
+++ src/routes/dataCenterDashboardRoute.js
@@ -2,7 +2,9 @@ const dataCenterDashboardController = require('../controllers/dataCenterDashboar
 
 const dataCenterDashboardRoute = (app) => {   
     // DC page
+    app.get('/Dashboard/Data-Center/years', dataCenterDashboardController.getAllYear);
     app.get('/Dashboard/Data-Center/carbon-emissions/:company_id/:year', dataCenterDashboardController.getAllCarbonEmissionsData);
+    app.get('/Dashboard/Data-Center/energy-consumption/:company_id/:year', dataCenterDashboardController.getAllEnergyConsumptionData);
 
     // overview page
     app.get('/Dashboard/sustainability-goals/:company_id', dataCenterDashboardController.getAllSustainabilityGoalsData);
