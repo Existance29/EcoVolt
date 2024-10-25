516e256394b28c5200274b9d7b24af85c9c09112
NgJoeYi <s10262850@connect.np.edu.sg>
2024-10-25 08:14:00 +0800

    updated url

--- src/routes/dataCenterDashboardRoute.js
+++ src/routes/dataCenterDashboardRoute.js
@@ -1,8 +1,11 @@
 const dataCenterDashboardController = require('../controllers/dataCenterDashboardController.js');
 
 const dataCenterDashboardRoute = (app) => {   
-    app.get('/Dashboard/Data-Center/carbon-emissions/:company_id/:date', dataCenterDashboard.getAllCarbonEmissionsData);
-    app.get('/Dashboard/Data-Center/sustainability-goals/:company_id/:date', dataCenterDashboard.getAllSustainabilityGoalsData);
+    // DC page
+    app.get('/Dashboard/Data-Center/carbon-emissions/:company_id/:date', dataCenterDashboardController.getAllCarbonEmissionsData);
+
+    // overview page
+    app.get('/Dashboard/sustainability-goals/:company_id', dataCenterDashboardController.getAllSustainabilityGoalsData);
 }
 
 module.exports = dataCenterDashboardRoute;
