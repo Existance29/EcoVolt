926b441279b2f56d16db8b1ce802e7e177f11419
exihs <54537576+Existance29@users.noreply.github.com>
2024-11-02 22:28:11 +0800

    more dashboard progress

--- src/public/scripts/pages/cellTowerDashboard.js
+++ src/public/scripts/pages/cellTowerDashboard.js
@@ -1,54 +1,95 @@
-Chart.defaults.font.size = 15
+Chart.defaults.font.size = 13
 Chart.defaults.animation = true
 Chart.defaults.color = "#CAC9CA"
 var hourArray = [10, 20, 30, 40, 50]
 var honeyPerMin = [5,10,15,20, 13]
 
-const honeyPerMinCanvas = document.getElementById('honeyPerMinChart')
-new Chart(honeyPerMinCanvas, {
-    type: 'line',
-    data: {
-        labels: hourArray,
-        datasets: [{
-            data: honeyPerMin,
-            borderColor: '#4FD1C5',
-            tension: 0.4,
-            fill: {
-                target: 'origin',
-                above: (context) => {
-                    const {ctx, chartArea} = context.chart
-                    let gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
-                    gradient.addColorStop(0, 'rgba(79, 209, 197, 0)')
-                    gradient.addColorStop(1, 'rgba(79, 209, 197, 0.5)')
-                    return gradient
+function renderLineChart(canvasElement, xData, yData, lineColor){
+    // const parentElement = canvasElement.parentElement
+    // const parentHeightPX = parentElement.offsetHeight
+    // //convert from px to vh
+    // const parentHeightVH = (100 * parentHeightPX / window.innerHeight)
+    // console.log(parentHeightPX)
+    // parentElement.style.maxHeight = `${parentHeightPX}px`
+    new Chart(canvasElement, {
+        type: 'line',
+        data: {
+            labels: yData,
+            datasets: [{
+                data: xData,
+                borderColor: lineColor,
+                tension: 0.4,
+                fill: {
+                    target: 'origin',
+                    above: (context) => {
+                        const {ctx, chartArea} = context.chart
+                        let gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
+                        gradient.addColorStop(0, lineColor+"00")
+                        gradient.addColorStop(1, lineColor+"80")
+                        return gradient
+                    }
                 }
-            }
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            y: {
-                grid: {
-                    color: "#E2E8F0",
-                    borderDash: [8, 4],
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    grid: {
+                        color: "#E2E8F0",
+                        borderDash: [8, 4],
+                    },
+                    ticks: {
+                        maxTicksLimit: 6,
+                        autoSkip: false,
+                    }
                 },
-                ticks: {
-                    maxTicksLimit: 6,
-                    autoSkip: false,
+                x: {
+                    grid: {
+                        display: false
+                    }
                 }
             },
-            x: {
-                grid: {
+            plugins:{
+                legend: {
                     display: false
                 }
-            }
+            },
+        }
+    })
+}
+
+function renderDoughnutChart(element, labels, data, colors){
+    new Chart(element, {
+        type: 'doughnut',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    data: data,
+                    backgroundColor: colors,
+                    borderWidth: 0,
+                    borderRadius: 0
+                }
+            ]
         },
-        plugins:{
-            legend: {
-                display: false
-            }
-        },
-    }
-})
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins:{
+                legend: {
+                    display: false
+                },
+            },
+            cutout: "65%"
+        }
+    })
+}
+
+renderLineChart(document.getElementById('carbonEmissionChart'), honeyPerMin, hourArray, "#4FD1C5")
+
+const energyBreakdownColors = ["#263332","#485251","#4FD1C5","#95D1CB","#5BA79F"]
+const energyBreakdownLabels = ["Gathering", "Converting", "Bug Runs", "Misc"]
+const energyBreakdownData = [1,2,3,4]
+renderDoughnutChart(document.getElementById('energyBreakdownChart'), energyBreakdownColors, energyBreakdownData, energyBreakdownColors)
