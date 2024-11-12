f6d12a4fc60583d66ed7057c3531cd3889b5acb7
3Kr15 <s10260462@connect.np.edu.sg>
2024-11-12 14:39:04 +0800

    comments and posts function added

--- src/controllers/activityController.js
+++ src/controllers/activityController.js
@@ -50,12 +50,66 @@ const toggleDislike = async (req, res) => {
         res.status(200).json({ dislikeCount: updatedDislikeCount });
     } catch (error) {
         console.error("Error toggling dislike: ", error);
-        res.status(500).json({ error: "Failed tp toggle dilike" });
+        res.status(500).json({ error: "Failed to toggle dislike" });
     }
 };
+
+const getCommentsByPostId = async (req, res) => {
+    const post_id = parseInt(req.params.post_id);
+    console.log(`postId: ${post_id}`);
+    try {
+        const comments = await activityModel.getCommentsByPostId(post_id);
+        if (!comments) {
+            return res.status(404).send({ message: "No comments found" });
+        }
+        res.status(200).json(comments);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "failed to fetch comments" });
+    }
+};
+
+const addNewComment = async (req, res) => {
+    const { post_id, user_id, company_id, comment_text } = req.body;
+    try {
+        const newComment = await activityModel.addNewComment(post_id, user_id, company_id, comment_text);
+        const updatedComments = await activityModel.getCommentsByPostId(post_id);
+        if (newComment) {
+            res.status(200).json({newComment, commentsCount: updatedComments.length});
+        } else {
+            res.status(500).json({ error: "Failed to add comment"})
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "failed to post new comment" });
+    }
+};
+
+const addNewPost = async (req, res) => {
+    const { user_id, company_id, context, media_url, carbon_emission, energy_consumption, activity_type, location} = req.body;
+
+    try {
+        const newPost = await activityModel.addNewPost(
+            user_id, company_id, context, media_url, carbon_emission, energy_consumption, activity_type, location
+        );
+
+        if (newPost) {
+            res.status(201).json({ message: "Post created successfully", post_id: newPost.post_id });
+        } else {
+            res.status(500).json({ error: "Failed to create post" });
+        }
+    } catch (error) {
+        console.error("Error creating post: ", error);
+        res.status(500).json({ error: "Failed to create post" });
+    }
+}
 
 module.exports = {
     getAllPosts,
     toggleLike,
     toggleDislike,
+    getCommentsByPostId,
+    addNewComment,
+    addNewPost,
 }
--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -4,7 +4,9 @@ const activityRoute = (app) => {
     app.get('/posts', activityController.getAllPosts);
     app.post('/toggleLike', activityController.toggleLike);
     app.post('/toggleDislike', activityController.toggleDislike);
-
+    app.get('/comment/:post_id', activityController.getCommentsByPostId);
+    app.post('/newComments', activityController.addNewComment);
+    app.post('/addPost', activityController.addNewPost);
 }
 
 module.exports = activityRoute;
