d112dbafdc1b164a439b20f3704e25fbdefc8ee4
exihs <54537576+Existance29@users.noreply.github.com>
2024-11-12 12:59:46 +0800

    settings
    almost done wtih accoun t

--- src/public/scripts/pages/settings.js
+++ src/public/scripts/pages/settings.js
@@ -1,13 +1,37 @@
+const accountTextFields = ["name", "email", "about"]
+
+function closeTextBox(editBtn, textbox){
+    textbox.classList.remove("active")
+    editBtn.innerText = "Edit"
+    textbox.disabled = true
+    console.log("a")
+}
+
+function getTextBox(editBtn){
+    return editBtn.parentElement.parentElement.children[1]
+}
+
+async function updateAccountInfo(){
+    let updateData = {}
+    accountTextFields.forEach(x => updateData[x] = document.getElementById(x).value)
+    const resp = await put("users/account/private", updateData)
+    //check if error
+    if (resp.status == 200) return true
+
+    //show error message
+    return false
+
+}
 
 //add event handler to text input edit buttons
 Array.from(document.getElementsByClassName("edit")).forEach(x => {
-    x.addEventListener("click", (event) => {
-        const textbox = x.parentElement.parentElement.children[1]
+    x.addEventListener("click", async() => {
+        const textbox = getTextBox(x)
         if (textbox.classList.contains("active")){
-            textbox.classList.remove("active")
-            x.innerText = "Edit"
-            textbox.disabled = true
+            if (await updateAccountInfo()) closeTextBox(x, textbox)
         } else{
+            //close all textboxes
+            Array.from(document.getElementsByClassName("edit")).forEach(y => closeTextBox(y, getTextBox(y)))
             textbox.classList.add("active")
             x.innerText = "Save"
             textbox.disabled = false
@@ -16,8 +40,8 @@ Array.from(document.getElementsByClassName("edit")).forEach(x => {
             var val = textbox.value
             textbox.value = '' 
             textbox.value = val 
+            //close all other active textboxes
         }
-        console.log(textbox)
     } )
 })
 
@@ -25,7 +49,6 @@ const navItems = document.getElementsByTagName("nav")[0].children
 
 //deal with the tab navigation
 function navClicked(navItem){
-    console.log(navItem.dataset.tab)
     //hide all tabs
     Array.from(document.getElementsByClassName("settings-container")).forEach(x => x.style.display = "none" )
 
@@ -42,3 +65,13 @@ Array.from(navItems).forEach(x => {
 
 //load the first tab
 navClicked(navItems[0])
+
+//load the setting data
+async function loadData(){
+    const accountData = await (await get("users/account/private")).json()
+
+    //load inputs
+    accountTextFields.forEach(x => document.getElementById(x).value = accountData[x])
+}
+
+loadData()
