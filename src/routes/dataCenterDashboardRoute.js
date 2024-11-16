3bc4692afdb7b57b8be714d9b7b14af8b89a50ca
NgJoeYi <s10262850@connect.np.edu.sg>
2024-11-16 23:03:13 +0800

    added jwt and page require sign in to dc

--- src/routes/dataCenterDashboardRoute.js
+++ src/routes/dataCenterDashboardRoute.js
@@ -1,51 +1,52 @@
 const dataCenterDashboardController = require('../controllers/dataCenterDashboardController.js');
+const authenticateToken = require("../middlewares/authenticateToken")
 
 const dataCenterDashboardRoute = (app) => {   
     // DC page
 
-    app.get('/Dashboard/Data-Center/:company_id', dataCenterDashboardController.getAllDataCenter);
-    app.get('/Dashboard/Data-Center/AvailableDates/:company_id/:dc?', dataCenterDashboardController.getAllDates);
+    app.get('/Dashboard/Data-Center', authenticateToken, dataCenterDashboardController.getAllDataCenter);
+    app.get('/Dashboard/Data-Center/AvailableDates/:dc?', authenticateToken, dataCenterDashboardController.getAllDates);
     
-    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/company/:company_id', dataCenterDashboardController.getTotalEnergyConsumptionByCompanyId); // text values 
-    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/data-center/:data_center_id', dataCenterDashboardController.getTotalEnergyConsumptionByDataCenterId);
-    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/data-center/:data_center_id/date', dataCenterDashboardController.getTotalEnergyConsumptionByDataCenterIdAndDate);
-    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/company/:company_id/date', dataCenterDashboardController.getTotalEnergyConsumptionByCompanyIdAndDate);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/company', authenticateToken, dataCenterDashboardController.getTotalEnergyConsumptionByCompanyId); // text values 
+    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/data-center/:data_center_id', authenticateToken, dataCenterDashboardController.getTotalEnergyConsumptionByDataCenterId);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/data-center/:data_center_id/date', authenticateToken,dataCenterDashboardController.getTotalEnergyConsumptionByDataCenterIdAndDate);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/Sum/company/date', authenticateToken,dataCenterDashboardController.getTotalEnergyConsumptionByCompanyIdAndDate);
     
-    app.get('/Dashboard/Data-Center/EnergyConsumption/company/:company_id', dataCenterDashboardController.getAllEnergyConsumptionByCompanyId);
-    app.get('/Dashboard/Data-Center/EnergyConsumption/data-center/:data_center_id', dataCenterDashboardController.getAllEnergyConsumptionByDataCenterId);
-    app.get('/Dashboard/Data-Center/EnergyConsumption/data-center/:data_center_id/date', dataCenterDashboardController.getAllEnergyConsumptionByDataCenterIdAndDate);
-    app.get('/Dashboard/Data-Center/EnergyConsumption/company/:company_id/date', dataCenterDashboardController.getAllEnergyConsumptionByCompanyIdAndDate);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/company', authenticateToken, dataCenterDashboardController.getAllEnergyConsumptionByCompanyId);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/data-center/:data_center_id', authenticateToken, dataCenterDashboardController.getAllEnergyConsumptionByDataCenterId);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/data-center/:data_center_id/date', authenticateToken, dataCenterDashboardController.getAllEnergyConsumptionByDataCenterIdAndDate);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/company/date', authenticateToken, dataCenterDashboardController.getAllEnergyConsumptionByCompanyIdAndDate);
 
 
-    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/company/:company_id', dataCenterDashboardController.getTotalCarbonEmissionByCompanyId); // text values
-    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/data-center/:data_center_id', dataCenterDashboardController.getTotalCarbonEmissionByDataCenterId);
-    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/company/:company_id/date', dataCenterDashboardController.getTotalCarbonEmissionByCompanyIdAndDate);
-    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/data-center/:data_center_id/date', dataCenterDashboardController.getTotalCarbonEmissionByDataCenterIdAndDate);
+    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/company', authenticateToken, dataCenterDashboardController.getTotalCarbonEmissionByCompanyId); // text values
+    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/data-center/:data_center_id', authenticateToken, dataCenterDashboardController.getTotalCarbonEmissionByDataCenterId);
+    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/company/date', authenticateToken, dataCenterDashboardController.getTotalCarbonEmissionByCompanyIdAndDate);
+    app.get('/Dashboard/Data-Center/CarbonEmission/Sum/data-center/:data_center_id/date', authenticateToken, dataCenterDashboardController.getTotalCarbonEmissionByDataCenterIdAndDate);
 
 
-    app.get('/Dashboard/Data-Center/CarbonEmission/company/:company_id', dataCenterDashboardController.getAllCarbonEmissionByCompanyId); // line chart 
-    app.get('/Dashboard/Data-Center/CarbonEmission/data-center/:data_center_id', dataCenterDashboardController.getAllCarbonEmissionByDataCenterId);
-    app.get('/Dashboard/Data-Center/CarbonEmission/company/:company_id/date', dataCenterDashboardController.getAllCarbonEmissionByCompanyIdAndDate);
-    app.get('/Dashboard/Data-Center/CarbonEmission/data-center/:data_center_id/date', dataCenterDashboardController.getAllCarbonEmissionByDataCenterAndDate);
+    app.get('/Dashboard/Data-Center/CarbonEmission/company', authenticateToken, dataCenterDashboardController.getAllCarbonEmissionByCompanyId); // line chart 
+    app.get('/Dashboard/Data-Center/CarbonEmission/data-center/:data_center_id', authenticateToken, dataCenterDashboardController.getAllCarbonEmissionByDataCenterId);
+    app.get('/Dashboard/Data-Center/CarbonEmission/company/date', authenticateToken, dataCenterDashboardController.getAllCarbonEmissionByCompanyIdAndDate);
+    app.get('/Dashboard/Data-Center/CarbonEmission/data-center/:data_center_id/date', authenticateToken, dataCenterDashboardController.getAllCarbonEmissionByDataCenterAndDate);
 
 
-    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/company/:company_id', dataCenterDashboardController.getTotalRenewableEnergyByCompanyId);
-    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/data-center/:data_center_id', dataCenterDashboardController.getTotalRenewableEnergyByDataCenterId);
-    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/data-center/:data_center_id/date', dataCenterDashboardController.getTotalRenewableEnergyByDataCenterIdAndDate);
-    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/company/:company_id/date', dataCenterDashboardController.getTotalRenewableEnergyByCompanyIdAndDate);
+    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/company', authenticateToken, dataCenterDashboardController.getTotalRenewableEnergyByCompanyId);
+    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/data-center/:data_center_id', authenticateToken, dataCenterDashboardController.getTotalRenewableEnergyByDataCenterId);
+    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/data-center/:data_center_id/date', authenticateToken, dataCenterDashboardController.getTotalRenewableEnergyByDataCenterIdAndDate);
+    app.get('/Dashboard/Data-Center/RenewableEnergy/Total/company/date', authenticateToken, dataCenterDashboardController.getTotalRenewableEnergyByCompanyIdAndDate);
     
-    app.get('/Dashboard/Data-Center/EnergyConsumption/GroupByDc/:company_id/:data_center_id?', dataCenterDashboardController.getEnergyConsumptionGroupByDc);
+    app.get('/Dashboard/Data-Center/EnergyConsumption/GroupByDc/:data_center_id?', authenticateToken, dataCenterDashboardController.getEnergyConsumptionGroupByDc);
 
-    app.get('/Dashboard/Data-Center/Devices/:company_id', dataCenterDashboardController.getDevicesCountByCompanyId);
-    app.get('/Dashboard/Data-Center/Devices/:company_id/:data_center_id', dataCenterDashboardController.getDevicesCountByCompanyIdAndDc);
+    app.get('/Dashboard/Data-Center/Devices', authenticateToken, dataCenterDashboardController.getDevicesCountByCompanyId);
+    app.get('/Dashboard/Data-Center/Devices/:data_center_id', authenticateToken, dataCenterDashboardController.getDevicesCountByCompanyIdAndDc);
     
-    app.get('/Dashboard/Data-Center/DevicesTypes/:company_id', dataCenterDashboardController.getDeviceTypesByCompanyId);
-    app.get('/Dashboard/Data-Center/DevicesTypes/:company_id/:data_center_id', dataCenterDashboardController.getDeviceTypesByCompanyIdAndDataCenter);
+    app.get('/Dashboard/Data-Center/DevicesTypes', authenticateToken, dataCenterDashboardController.getDeviceTypesByCompanyId);
+    app.get('/Dashboard/Data-Center/DevicesTypes/:data_center_id', authenticateToken, dataCenterDashboardController.getDeviceTypesByCompanyIdAndDataCenter);
 
 
     // app.post('/chat', dataCenterDashboardController.getChatbotResponse);
     // overview page
-    app.get('/Dashboard/sustainability-goals/:company_id', dataCenterDashboardController.getAllSustainabilityGoalsData);
+    app.get('/Dashboard/sustainability-goals', authenticateToken, dataCenterDashboardController.getAllSustainabilityGoalsData);
 }
 
 module.exports = dataCenterDashboardRoute;
